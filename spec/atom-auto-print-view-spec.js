fbe5b70a90fe51c72c509e3663ae037b18d8e717
David Olson <deolson@pugetsound.edu>
2017-03-29 18:28:45 -0700

    init commit

---
+++ spec/atom-auto-print-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomAutoPrintView from '../lib/atom-auto-print-view';
+
+describe('AtomAutoPrintView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
