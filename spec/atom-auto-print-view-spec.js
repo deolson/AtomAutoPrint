db632888bfb003a52f70e87baa02d6616052d665
David Olson <deolson@pugetsound.edu>
2017-04-09 21:19:14 -0700

    now queries google on highlighted text for stackoverflow answers, also restructed some pieces not being used

--- spec/atom-auto-print-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomAutoPrintView from '../lib/atom-auto-print-view';
-
-describe('AtomAutoPrintView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
